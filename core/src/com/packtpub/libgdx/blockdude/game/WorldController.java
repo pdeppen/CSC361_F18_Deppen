2f75f0e702d47a5a9a6dfc57e4e8bf3552f4459a
pdeppen <pdeppen17@aol.com>
2018-10-29 22:11:31 -0400

    created world controller

---
+++ core/src/com/packtpub/libgdx/blockdude/game/WorldController.java
@@ -0,0 +1,38 @@
+package com.packtpub.libgdx.blockdude.game;
+
+/**
+ * Created by Philip Deppen (Milestone 1, 10/29/18)
+ * contains game logic
+ */
+public class WorldController 
+{
+	private static final String TAG = WorldController.class.getName();
+	
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 * constructor
+	 */
+	public WorldController() 
+	{
+		
+	}
+	
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 * used for reseting game objects instead of making new ones
+	 */
+	private void init()
+	{
+		
+	}
+	
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 * contains game logic is called several hundred times per second
+	 * @param deltaTime
+	 */
+	public void update (float deltaTime)
+	{
+		
+	}
+}
