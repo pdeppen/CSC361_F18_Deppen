94bd60555836dc8b157d87774636f74821f7d70c
pdeppen <pdeppen17@aol.com>
2018-10-29 21:40:48 -0400

    added constants class

---
+++ core/src/com/packtpub/libgdx/blockdude/util/Constants.java
@@ -0,0 +1,14 @@
+package com.packtpub.libgdx.blockdude.util;
+
+/**
+ * Created by Philip Deppen (Milestone 1, 10/29/18)
+ * holds values that are fixed throughout project
+ */
+public class Constants 
+{
+	// Visible game world is 5 meters wide
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	
+	// Visible game world is 5 meters tall
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+}
