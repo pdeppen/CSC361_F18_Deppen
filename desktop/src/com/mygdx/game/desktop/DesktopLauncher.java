c5753f34f3042dc252e161036d21789f06f5f4f7
pdeppen <pdeppen17@aol.com>
2018-10-30 16:34:39 -0400

    created texture atlas

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,10 +3,34 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.packtpub.libgdx.blockdude.main.BlockDudeMain;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+/**
+ * Created by Philip Deppen (Milestone 1, 10/30/18)
+ */
+
+public class DesktopLauncher 
+{
+	private static boolean rebuildAtlas = false;
+	private static boolean drawDebugOutline = true;
+	
+	public static void main (String[] arg) 
+	{
+		if (rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			
+			TexturePacker.process(settings, "assets-raw/images", "../core/assets/images", "blockdude.pack");
+		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "BlockDude";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new BlockDudeMain(), config);
 	}
 }
