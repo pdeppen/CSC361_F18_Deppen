16194ec501c58ba6417cd715dfaf0c826bff0f99
pdeppen <pdeppen17@aol.com>
2018-10-29 21:59:57 -0400

    created blockdudemain

---
+++ core/src/com/packtpub/libgdx/blockdude/main/BlockDudeMain.java
@@ -0,0 +1,50 @@
+package com.packtpub.libgdx.blockdude.main;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
+
+/**
+ * Created by Philip Deppen (Milestone 1, 10/29/18)
+ * Contains game loop and handles major aspects of game
+ */
+public class BlockDudeMain implements ApplicationListener {
+
+	private static final String TAG = BlockDudeMain.class.getName();
+	
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
