a97b59d4bb89af84243095cb5604669eed472003
pdeppen <pdeppen17@aol.com>
2018-10-29 22:46:18 -0400

    built game loop

--- core/src/com/packtpub/libgdx/blockdude/main/BlockDudeMain.java
+++ core/src/com/packtpub/libgdx/blockdude/main/BlockDudeMain.java
@@ -1,6 +1,11 @@
 package com.packtpub.libgdx.blockdude.main;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.packtpub.libgdx.blockdude.game.WorldController;
+import com.packtpub.libgdx.blockdude.game.WorldRenderer;
 
 /**
  * Created by Philip Deppen (Milestone 1, 10/29/18)
@@ -10,40 +15,83 @@ public class BlockDudeMain implements ApplicationListener {
 
 	private static final String TAG = BlockDudeMain.class.getName();
 	
+	private WorldController worldController;
+    private WorldRenderer worldRenderer;
+    private boolean paused;
+    
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 */
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
+		// Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		
+		// Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);	
+		
+		// Game world is active on start
+		paused = false;
+	}
+
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 */
+	@Override
+	public void resize(int width, int height) 
+	{
+		worldRenderer.resize(width, height);
+	}
+
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 */
+	@Override
+	public void render() 
+	{
+		// Do not update game when paused
+		if (!paused) 
+		{
+			// Update game world by the time that has passed
+			// since last rendered frame.
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		
+		// Sets the clear screen color to: Cornflower Blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		// Clears the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// Render game world to screen
+		worldRenderer.render();
 		
 	}
 
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 */
 	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+	public void pause() 
+	{
+		paused = true;
 	}
 
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 */
 	@Override
-	public void render() {
-		// TODO Auto-generated method stub
-		
+	public void resume() 
+	{
+		paused = false;
 	}
 
+	/**
+	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 */
 	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
+	public void dispose() 
+	{
+		worldRenderer.dispose();
 	}
 
 }
