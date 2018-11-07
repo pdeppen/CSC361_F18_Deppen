f2bcc01e175f84711cf0448a0f313125f98107e6
pdeppen <pdeppen17@aol.com>
2018-11-06 20:18:32 -0500

    #29 - finished

--- core/src/com/packtpub/libgdx/blockdude/game/WorldController.java
+++ core/src/com/packtpub/libgdx/blockdude/game/WorldController.java
@@ -13,20 +13,24 @@ import com.packtpub.libgdx.blockdude.game.Assets;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Input.Keys;
-
+import com.packtpub.libgdx.blockdude.game.objects.Ground;
+import com.packtpub.libgdx.blockdude.util.Constants;
 
 /**
  * Created by Philip Deppen (Milestone 1, 10/29/18)
+ * Edited by Philip Deppen (Milestone 2, 11/6/18, issue 29)
  * contains game logic
  */
 public class WorldController extends InputAdapter
 {
 	private static final String TAG = WorldController.class.getName();
 	
-	public Sprite[] testSprites;
-	public int selectedSprite;
 	public CameraHelper cameraHelper;
 
+	public Level level;
+	public int lives;
+	public int score;
+	
 	
 	/**
 	 * Created by Philip Deppen (Milestone 1, 10/29/18)
@@ -39,13 +43,24 @@ public class WorldController extends InputAdapter
 	
 	/**
 	 * Created by Philip Deppen (Milestone 1, 10/29/18)
+	 * Edited by Philip Deppen (Milestone 2, 11/6/18, issue 29)
 	 * used for reseting game objects instead of making new ones
 	 */
 	private void init()
 	{
 		Gdx.input.setInputProcessor(this);
 		cameraHelper = new CameraHelper();
-		initTestObjects();
+		lives = Constants.LIVES_START;
+		initLevel();
+	}
+	
+	/**
+	 * Created by Philip Deppen (Milestone 2, 11/6/18, issue 29)
+	 */
+	private void initLevel()
+	{
+		score = 0;
+		level = new Level (Constants.LEVEL_01);
 	}
 	
 	/**
@@ -56,47 +71,9 @@ public class WorldController extends InputAdapter
 	public void update (float deltaTime)
 	{
 		handleDebugInput(deltaTime);
-		updateTestObjects(deltaTime);
 		cameraHelper.update(deltaTime);
 	}
 	
-	/**
-	 * Created by Philip Deppen (Milestone 1, 10/30/18, issue 13)
-	 * Edited by Philip Deppen (Milestone 2, 11/5/18, issue 20)
-	 * creates test sprites
-	 */
-	private void initTestObjects()
-	{
-		// Create new array for 5 sprites
-		testSprites = new Sprite[15];
-		
-		//Create a list of texture regions
-		Array<TextureRegion> regions = new Array<TextureRegion>();
-		regions.add(Assets.instance.dude.dude);
-		regions.add(Assets.instance.star.star);
-		regions.add(Assets.instance.block.block);
-		regions.add(Assets.instance.brokenBlock.brokenBlock);
-		regions.add(Assets.instance.movingBlock.movingBlock);
-		regions.add(Assets.instance.door.door);
-		regions.add(Assets.instance.ground.ground);
-		
-		// Create new sprites using the just created texture
-		for (int i = 0; i < testSprites.length; i++) {
-			Sprite spr = new Sprite(regions.random());
-			// Define sprite size to be 1m x 1m in game world
-			spr.setSize(1, 1);
-			// Set origin to sprite's center
-			spr.setOrigin(spr.getWidth() / 2.0f, spr.getHeight() / 2.0f);
-			// Calculate random position for sprite
-			float randomX = MathUtils.random(-2.0f, 2.0f);
-			float randomY = MathUtils.random(-2.0f, 2.0f);
-			spr.setPosition(randomX, randomY);
-			// Put new sprite into array
-			testSprites[i] = spr;
-		}
-		// Set first sprite as selected one
-		selectedSprite = 0;
-	}
 		
 	/**
 	 * Created by Philip Deppen (Milestone 1, 10/30/18, issue 13)
@@ -106,21 +83,7 @@ public class WorldController extends InputAdapter
 	private void handleDebugInput (float deltaTime) {
 		if (Gdx.app.getType() != ApplicationType.Desktop) 
 			return;
-		
-		// Selected Sprite Controls
-		float sprMoveSpeed = 5 * deltaTime;
-		if (Gdx.input.isKeyPressed(Keys.A)) 
-			moveSelectedSprite(-sprMoveSpeed, 0);
-		
-		if (Gdx.input.isKeyPressed(Keys.D))
-			moveSelectedSprite(sprMoveSpeed, 0);
-		
-		if (Gdx.input.isKeyPressed(Keys.W))
-			moveSelectedSprite(0, sprMoveSpeed);
-		
-		if (Gdx.input.isKeyPressed(Keys.S)) 
-			moveSelectedSprite(0, -sprMoveSpeed);
-		
+				
 		// Camera Controls (move)
 		float camMoveSpeed = 5 * deltaTime;
 		float camMoveSpeedAccelerationFactor = 5;
@@ -162,30 +125,6 @@ public class WorldController extends InputAdapter
 		cameraHelper.setPosition(x, y);
 	}
 	
-	/**
-	 * Created by Philip Deppen (Milestone 1, 10/30/18, issue 13)
-	 * @param x
-	 * @param y
-	 */
-	private void moveSelectedSprite (float x, float y) 
-	{
-		testSprites[selectedSprite].translate(x, y);
-	}
-	
-	/**
-	 * Created by Philip Deppen (Milestone 1, 10/30/18, issue 13)
-	 * @param deltaTime
-	 */
-	private void updateTestObjects(float deltaTime) {
-		// Get current rotation from selected sprite
-		float rotation = testSprites[selectedSprite].getRotation();
-		// Rotate sprite by 90 degrees per second
-		rotation += 90 * deltaTime;
-		// Wrap around at 360 degrees
-		rotation %= 360;
-		// Set new rotation value to selected sprite
-		testSprites[selectedSprite].setRotation(rotation);
-	}
 	
 	/**
 	 * Created by Philip Deppen (Milestone 1, 10/30/18, issue 13)
@@ -197,23 +136,7 @@ public class WorldController extends InputAdapter
 			init();
 			Gdx.app.debug(TAG, "Game world resetted");
 		}
-		// Select next sprite
-		else if (keycode == Keys.SPACE) {
-			selectedSprite = (selectedSprite + 1) % testSprites.length;
-			// Update camera's target to follow the currently 
-			// selected sprite
-			if (cameraHelper.hasTarget()) {
-				cameraHelper.setTarget(testSprites[selectedSprite]);
-			}
-			Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
-		}
-		// Toggle camera follow 
-		else if (keycode == Keys.ENTER) {
-			cameraHelper.setTarget(cameraHelper.hasTarget() ? null : 
-				testSprites[selectedSprite]);
-			Gdx.app.debug(TAG, "Camera follow enabled: " + 
-				cameraHelper.hasTarget());
-		}
+
 		return false;
 	}
 }
