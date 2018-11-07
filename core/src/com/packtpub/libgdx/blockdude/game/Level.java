98e06d1c7bc1fdd31824ffb6eb454a43604c4679
pdeppen <pdeppen17@aol.com>
2018-11-06 19:55:55 -0500

    #26 - created level loader. still needs work

---
+++ core/src/com/packtpub/libgdx/blockdude/game/Level.java
@@ -0,0 +1,152 @@
+package com.packtpub.libgdx.blockdude.game;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.packtpub.libgdx.blockdude.game.objects.AbstractGameObject;
+import com.packtpub.libgdx.blockdude.game.objects.Ground;
+import com.packtpub.libgdx.blockdude.game.Level.BLOCK_TYPE;
+import com.packtpub.libgdx.blockdude.game.objects.Ground;
+
+/**
+ * Created by Philip Deppen (Milestone 2, 11/6/18, issue 26)
+ */
+public class Level 
+{
+	public static final String TAG = Level.class.getName();
+	
+	/**
+	 * Created by Philip Deppen (Milestone 2, 11/6/18, issue 26)
+	 * this enum represents entire game world objects
+	 */
+	public enum BLOCK_TYPE 
+	{
+		EMPTY (0, 0, 0), // black
+		GROUND (0, 255, 0), // green
+		PLAYER_SPAWNPOINT (255, 255, 255), // white
+		ITEM_STAR (255, 0, 255); // purple
+	
+		private int color;
+		
+		private BLOCK_TYPE (int r, int g, int b)
+		{
+			color = r << 24 | g << 16 | b << 8 | 0xff;
+		}
+		
+		public boolean sameColor (int color)
+		{
+			return this.color == color;
+		}
+		
+		public int getColor ()
+		{
+			return color;
+		}
+	}
+	
+	// objects 
+	public Array<Ground> ground;
+	
+	// decoration
+	
+	/**
+	 * Created by Philip Deppen (Milestone 2, 11/6/18, issue 26)
+	 * @param filename
+	 */
+	public Level (String filename)
+	{
+		init(filename);
+	}
+	
+	/**
+	 * Created by Philip Deppen (Milestone 2, 11/6/18, issue 26)
+	 * @param filename
+	 */
+	private void init (String filename) 
+	{
+		// objects
+		ground = new Array<Ground>();
+		
+		// load image file that represents the level data
+		Pixmap pixmap = new Pixmap (Gdx.files.internal(filename));
+		
+		// scan pixels from top-left to bottom-right
+		int lastPixel = -1;
+		
+		for (int pixelY = 0; pixelY < pixmap.getHeight(); pixelY++)
+		{
+			for (int pixelX = 0; pixelX < pixmap.getWidth(); pixelX++)
+			{
+				AbstractGameObject obj = null;
+				float offsetHeight = 0;
+				// height grows from bottom to top
+				float baseHeight = pixmap.getHeight() - pixelY;
+				// get color of current pixel as 32-bit RGBA value
+				int currentPixel = pixmap.getPixel(pixelX, pixelY);
+				
+				// find matching color value to identify block type at (x,y)
+				// point and create the corresponding game object if there is
+				// a match empty space
+				if (BLOCK_TYPE.EMPTY.sameColor(currentPixel)) 
+				{
+					// do nothing
+				}
+				// rock
+				else if (BLOCK_TYPE.GROUND.sameColor(currentPixel)) 
+				{
+					if (lastPixel != currentPixel) 
+					{
+						obj = new Ground();
+						float heightIncreaseFactor = 0.25f;
+						offsetHeight = -2.5f;
+						obj.position.set(pixelX, baseHeight * obj.dimension.y * heightIncreaseFactor + offsetHeight);
+						ground.add((Ground)obj);
+					} 
+					else
+					{
+						ground.get(ground.size - 1).increaseLength(1);
+					}
+				}
+				// player spawn point
+				else if
+				(BLOCK_TYPE.PLAYER_SPAWNPOINT.sameColor(currentPixel)) 
+				{
+				
+				}
+				// gold coin
+				else if (BLOCK_TYPE.ITEM_STAR.sameColor(currentPixel)) 
+				{
+				
+				}
+				// unknown object/pixel color
+				else {
+					int r = 0xff & (currentPixel >>> 24); //red color channel
+					int g = 0xff & (currentPixel >>> 16); //green color channel
+					int b = 0xff & (currentPixel >>> 8); //blue color channel
+					int a = 0xff & currentPixel; //alpha channel
+					Gdx.app.error(TAG, "Unknown object at x<" + pixelX + "> y<" + pixelY + ">: r<" + r+ "> g<" + g + "> b<" + b + "> a<" + a + ">");
+				}
+				lastPixel = currentPixel;
+			}
+		}
+		
+		// decoration
+		
+		// free memory
+		pixmap.dispose();
+		Gdx.app.debug(TAG, "level '" + filename + "' loaded");
+	}
+	
+	/**
+	 * Created by Philip Deppen (Milestone 2, 11/6/18, issue 26)
+	 * @param batch
+	 */
+	public void render (SpriteBatch batch) 
+	{
+		// draw ground
+		for (Ground grnd : ground)
+			grnd.render(batch);
+	}
+}
