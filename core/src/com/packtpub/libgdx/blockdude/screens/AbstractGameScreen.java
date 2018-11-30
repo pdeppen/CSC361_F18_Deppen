ce989a4516a847e677f8a006b01b10b9e4ca4bd4
pdeppen <pdeppen17@aol.com>
2018-11-30 18:50:18 -0500

    #54 - finished

---
+++ core/src/com/packtpub/libgdx/blockdude/screens/AbstractGameScreen.java
@@ -0,0 +1,35 @@
+package com.packtpub.libgdx.blockdude.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.packtpub.libgdx.blockdude.game.Assets;
+
+/**
+ * Created by Philip Deppen (Milestone 4, 11/30/18, issue 53)
+ */
+
+public abstract class AbstractGameScreen 
+{
+	protected Game game;
+	
+	public AbstractGameScreen (Game game)
+	{
+		this.game = game;
+	}
+	
+	public abstract void render (float deltaTime);
+	public abstract void resize (int width, int height);
+	public abstract void show();
+	public abstract void pause();
+	
+	public void resume ()
+	{
+		Assets.instance.init(new AssetManager());
+	}
+	
+	public void dispose ()
+	{
+		Assets.instance.dispose();
+	}
+}
